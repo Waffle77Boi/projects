fe844f7682a2a9ea9f15ca34321772eb587e7fff
Waffle77Boi <162144020+Waffle77Boi@users.noreply.github.com>
2024-05-26 22:38:54 +0200

    firstjava.js

--- firstjava.js
+++ firstjava.js
@@ -14,7 +14,7 @@ const numberOfLegs = (animal, count) => {
 }
 const legCount = [
     {animal: "pig", count: 15},
-    {animal:"cow", coumt: 45},
+    {animal:"cow", count: 45},
     {animal:"dog", count: 5},
     {animal:"sheep", count: 20},
     {animal: "chicken", count: 20},
@@ -22,4 +22,4 @@ const legCount = [
     {animal:"Humfry", count: 1},
 ].reduce((acc, {animal, count}) => acc += numberOfLegs(animal, count),0);
 
-console.log('You have'+${legCount}+'legs on your farm.');
+console.log(`You have ${legCount} legs on your farm.`);
